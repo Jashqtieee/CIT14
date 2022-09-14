0b2f15b9ee2470827cd69bf91acaa56c2e48bdeb
CIT142G-25@M306.UC-BCF.EDU.PH <CIT142G-25@M306.UC-BCF.EDU.PH>
2022-09-14 15:47:36 +0800

    html ni juswaaa

--- push.bat
+++ push.bat
@@ -2,5 +2,5 @@
 git remote set-url origin https://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@github.com/Jashqtieee/CIT14.git
 pause
 git add .
-git commit -m "Auto Upload"
+git commit -m "html ni juswaaa"
 git push
